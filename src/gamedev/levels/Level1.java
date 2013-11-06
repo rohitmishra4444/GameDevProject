c0a04f9e7c58b17a08bc4ec4ea28dbc121b98d98
Stefan Wanzenried <Wanze@Stefans-MacBook-Pro.local>
2013-11-06 21:19:48 +0100

    Rename class DinosaurWithPhysic to Dinosaur

--- src/gamedev/levels/Level1.java
+++ src/gamedev/levels/Level1.java
@@ -1,5 +1,5 @@
 package gamedev.levels;
-import gamedev.objects.DinosaurWithPhysic;
+import gamedev.objects.Dinosaur;
 import gamedev.scenes.LevelScene;
 
 import java.util.Random;
@@ -15,7 +15,7 @@ public class Level1 extends LevelScene {
 	public void createScene() {
 		Random r = new Random();
 		for (int i = 0; i < 10; i++) {
-			this.attachChild(new DinosaurWithPhysic(r.nextInt(1024), r
+			this.attachChild(new Dinosaur(r.nextInt(1024), r
 					.nextInt(1204)));
 		}
 		// this.attachChild(new Tree(300, 300));
