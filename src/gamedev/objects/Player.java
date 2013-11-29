a9e6a5422c4010dbe2f83ebdf29da0766239fb2d
Stefan Wanzenried <Wanze@Stefans-MacBook-Pro.local>
2013-11-29 18:31:30 +0100

    Add spears texture, fix animation duration

--- src/gamedev/objects/Player.java
+++ src/gamedev/objects/Player.java
@@ -18,7 +18,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 public class Player extends AnimatedSprite {
 
 	public final static long[] ANIMATION_DURATION = { 50, 50, 50, 50, 50, 50,
-			50, 50, 50, 50, 50 };
+			50, 50 };
 	public final static int FRAMES_PER_ANIMATION = 8;
 	public final static int TILES_PER_LINE = 16;
 
