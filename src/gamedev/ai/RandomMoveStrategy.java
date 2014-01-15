f7bdcd218fc5527a6a2dd94a80b15d795a1aa7d0
Stefan Wanzenried <Wanze@Stefans-MacBook-Pro.local>
2014-01-15 21:54:54 +0100

    Update RandomMoveStrategy. Set min/max x and y values for the points

--- src/gamedev/ai/RandomMoveStrategy.java
+++ src/gamedev/ai/RandomMoveStrategy.java
@@ -24,6 +24,13 @@ public class RandomMoveStrategy extends MoveStrategy {
 	/** Set to true if a point is reached */
 	protected boolean reachedGoal = false;
 	
+	protected float minX;
+	protected float maxX;
+	protected float minY;
+	protected float maxY;
+	
+	protected boolean isBounded = false;
+	
 	public RandomMoveStrategy(AnimatedObject object, float minDistance, float maxDistance) {
 		super(object);
 		this.minDistance = minDistance;
@@ -38,22 +45,40 @@ public class RandomMoveStrategy extends MoveStrategy {
 		this.waitBetweenTime = waitBetweenTime;
 		this.init();
 	}
+
+	public RandomMoveStrategy(AnimatedObject object, float minDistance, float maxDistance, float waitBetweenTime, float minX, float maxX, float minY, float maxY) {
+		super(object);
+		this.minDistance = minDistance;
+		this.maxDistance = maxDistance;
+		this.waitBetweenTime = waitBetweenTime;
+		this.minX = minX;
+		this.maxX = maxX;
+		this.minY = minY;
+		this.maxY = maxY;
+		this.isBounded = true;
+		this.init();
+	}
 	
 	protected void init() {
 		Vector2 rPoint = this.getRandomPoint();
 		Vector2 bodyPos = this.object.getBody().getPosition();
 		this.object.moveTo(rPoint, GameState.WALKING);
 		this.duration = calculateDuration(bodyPos, rPoint, this.object.getBody().getLinearVelocity());
-//		System.out.println("Move from " + bodyPos + " to random point " + rPoint + ", needed time is " + this.duration);
 	}
 	
 	protected Vector2 getRandomPoint() {
 		Random r = new Random();
 		Vector2 bodyPos = this.object.getBody().getPosition();
-		float x = this.minDistance + (r.nextFloat() * (this.maxDistance - this.minDistance));
-		float y = this.minDistance + (r.nextFloat() * (this.maxDistance - this.minDistance));
+		float x = bodyPos.x + this.minDistance + (r.nextFloat() * (this.maxDistance - this.minDistance));
+		float y = bodyPos.y + this.minDistance + (r.nextFloat() * (this.maxDistance - this.minDistance));
 		if (r.nextInt(2) == 0) x = -x;
 		if (r.nextInt(2) == 0) y = -y;
+		if (this.isBounded) {
+			x = Math.min(x, maxX);
+			x = Math.max(x, minX);
+			y = Math.min(y, maxY);
+			y = Math.max(y, minY);			
+		}
 		return new Vector2(bodyPos.x + x, bodyPos.y + y);
 	}
 	
