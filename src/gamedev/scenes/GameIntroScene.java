01eb54de6870bd0f9fb9d9c0613da00782f8299c
David Wettstein <dwettstein@gmail.com>
2014-01-20 11:39:28 +0100

    Worked on IntroScene.

--- src/gamedev/scenes/GameIntroScene.java
+++ src/gamedev/scenes/GameIntroScene.java
@@ -18,32 +18,48 @@ import org.andengine.util.HorizontalAlign;
 
 public class GameIntroScene extends BaseScene {
 
-	private Text gameIntroText;
+	private static final String string0 = "Once a day," + "\n"
+			+ "in the modern world of today..." + "\n\n\n" + "Tap...";
+	private static final String string1 = "Text1";
+	private static final String string2 = "Text2";
+	private static final String string3 = "Text3";
+	private static final String string4 = "Text4";
+	private static final String string5 = "Text5";
+	private static final String string6 = "Text6";
+
+	private static final String[] gameIntroStrings = { string0, string1,
+			string2, string3, string4, string5, string6 };
+
+	private ArrayList<Text> gameIntroTexts;
 	private ArrayList<Sprite> gameIntroSprites;
+	private int nextText = 1;
 	private int nextSprite = 0;
 
 	@Override
 	public void createScene() {
-		float centerX = camera.getCenterX()
-				- resourcesManager.game_intro_region.get(0).getWidth() / 2;
-		float centerY = camera.getCenterY()
-				- resourcesManager.game_intro_region.get(0).getHeight() / 2;
 
-		String gameIntroString = "Once a day," + "\n"
-				+ "in the modern world of today..." + "\n\n\n" + "Tap...";
-		gameIntroText = new Text(0, 0, resourcesManager.font, gameIntroString,
-				gameIntroString.length(), vbom);
-		gameIntroText.setPosition(
-				camera.getCenterX() - gameIntroText.getWidth() / 2,
-				camera.getCenterY() - gameIntroText.getHeight() / 2);
-		gameIntroText.setHorizontalAlign(HorizontalAlign.CENTER);
-		this.registerTouchArea(gameIntroText);
-		attachChild(gameIntroText);
+		gameIntroTexts = new ArrayList<Text>();
+
+		for (String string : gameIntroStrings) {
+			Text text = new Text(0, 0, resourcesManager.font, string,
+					string.length(), vbom);
+			text.setPosition(camera.getCenterX() - text.getWidth() / 2,
+					camera.getCenterY() - text.getHeight() / 2);
+			text.setHorizontalAlign(HorizontalAlign.CENTER);
+			gameIntroTexts.add(text);
+		}
+		attachChild(gameIntroTexts.get(0));
+		registerTouchArea(gameIntroTexts.get(0));
+
+		float spriteCenterX = camera.getCenterX()
+				- resourcesManager.game_intro_region.get(0).getWidth() / 2;
+		float spriteCenterY = camera.getCenterY()
+				- resourcesManager.game_intro_region.get(0).getHeight() / 2;
 
 		gameIntroSprites = new ArrayList<Sprite>();
 		for (ITextureRegion region : resourcesManager.game_intro_region) {
-			Sprite sprite = new Sprite(centerX, centerY, region.getWidth(),
-					region.getHeight(), region, vbom) {
+			Sprite sprite = new Sprite(spriteCenterX, spriteCenterY,
+					region.getWidth(), region.getHeight(), region, vbom) {
 				@Override
 				protected void preDraw(GLState pGLState, Camera pCamera) {
 					super.preDraw(pGLState, pCamera);
@@ -72,23 +88,30 @@ public class GameIntroScene extends BaseScene {
 					long touchTime = pSceneTouchEvent.getMotionEvent()
 							.getEventTime();
 
-					if (touchTime > lastTouchTime + WAIT_TIME
-							&& pTouchArea.equals(gameIntroText)) {
-						gameIntroText.detachSelf();
-						unregisterTouchArea(gameIntroText);
+					if (nextText == 0) {
+						return false;
+					}
 
-						Sprite newSprite = gameIntroSprites.get(0);
+					if (touchTime > lastTouchTime + WAIT_TIME
+							&& pTouchArea.equals(gameIntroTexts
+									.get(nextText - 1))) {
+						Text oldText = gameIntroTexts.get(nextText - 1);
+						oldText.detachSelf();
+						unregisterTouchArea(oldText);
+
+						Sprite newSprite = gameIntroSprites.get(nextSprite);
 						attachChild(newSprite);
 						newSprite
 								.registerEntityModifier(new FadeInModifier(1f));
 						registerTouchArea(newSprite);
+
 						lastTouchTime = touchTime;
 						nextSprite++;
 					}
 
 					// Show the GameMapScene if last sprite was touched, else
 					// show
-					// the next sprite.
+					// the next text.
 					if (touchTime > lastTouchTime + WAIT_TIME
 							&& pTouchArea.equals(gameIntroSprites
 									.get(gameIntroSprites.size() - 1))) {
@@ -108,15 +131,14 @@ public class GameIntroScene extends BaseScene {
 						oldSprite.detachSelf();
 						unregisterTouchArea(oldSprite);
 
-						Sprite newSprite = gameIntroSprites.get(nextSprite);
-						attachChild(newSprite);
-						newSprite
-								.registerEntityModifier(new FadeInModifier(1f));
-						registerTouchArea(newSprite);
+						Text newText = gameIntroTexts.get(nextText);
+						attachChild(newText);
+						registerTouchArea(newText);
 
 						lastTouchTime = touchTime;
-						nextSprite++;
+						nextText++;
 					}
+
 				}
 				return false;
 			}
