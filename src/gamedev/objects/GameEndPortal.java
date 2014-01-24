9605fb17eae61b618652679dfd1ec4c779574ebb
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2014-01-24 18:46:52 +0100

    Working on map

--- src/gamedev/objects/GameEndPortal.java
+++ src/gamedev/objects/GameEndPortal.java
@@ -19,36 +19,39 @@ public class GameEndPortal extends Sprite {
 	public GameEndPortal(float pX, float pY) {
 		super(pX, pY, ResourcesManager.getInstance().gameEndPortalRegion,
 				ResourcesManager.getInstance().vbom);
-		this.setAlpha(0.8f);
-		this.setScale(0.5f);
+//		this.setAlpha(0.8f);
+//		this.setScale(0.5f);
 	}
-
-	@Override
-	protected void onManagedUpdate(float pSecondsElapsed) {
-		super.onManagedUpdate(pSecondsElapsed);
-		if (res.avatar.collidesWith(this) && gameEndPortalContact == false) {
-			gameEndPortalContact = true;
-
-			int numberOfFinishedQuests = 0;
-			ArrayList<Quest> quests = SceneManager.getInstance()
-					.getCurrentGameMapScene().getQuests();
-
-			for (Quest quest : quests) {
-				if (quest.isFinished()) {
-					numberOfFinishedQuests++;
-				}
-			}
-			if (numberOfFinishedQuests == quests.size()) {
-				SceneManager.getInstance().loadGameEndScene(res.engine);
-				res.avatar.setState(GameState.IDLE, -1);
-			} else {
-				res.activity
-						.toastOnUIThread(
-								"Hmm... the portal is not working. Did I complete all quests?",
-								Toast.LENGTH_SHORT);
-			}
-		} else if (!this.collidesWith(res.avatar)) {
-			gameEndPortalContact = false;
-		}
-	}
+	
+	// I suggest to handle this logic with the last Quest. Since we must complete all quests to get to the last quest,
+	// This is no longer necessary. If Quest 5 (Make portal work) is finished, the game is finished
+	
+//	@Override
+//	protected void onManagedUpdate(float pSecondsElapsed) {
+//		super.onManagedUpdate(pSecondsElapsed);
+//		if (res.avatar.collidesWith(this) && gameEndPortalContact == false) {
+//			gameEndPortalContact = true;
+//
+//			int numberOfFinishedQuests = 0;
+//			ArrayList<Quest> quests = SceneManager.getInstance()
+//					.getCurrentGameMapScene().getQuests();
+//
+//			for (Quest quest : quests) {
+//				if (quest.isFinished()) {
+//					numberOfFinishedQuests++;
+//				}
+//			}
+//			if (numberOfFinishedQuests == quests.size()) {
+//				SceneManager.getInstance().loadGameEndScene(res.engine);
+//				res.avatar.setState(GameState.IDLE, -1);
+//			} else {
+//				res.activity
+//						.toastOnUIThread(
+//								"Hmm... the portal is not working. Did I complete all quests?",
+//								Toast.LENGTH_SHORT);
+//			}
+//		} else if (!this.collidesWith(res.avatar)) {
+//			gameEndPortalContact = false;
+//		}
+//	}
 }
